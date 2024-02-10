3dc21f6a1861590a6c2be8adfb617852338132b7
BenjaminMahmic <benjamin97mahmic@gmail.com>
2024-02-10 13:48:36 +0100

    Improved package to address Pub Points issues for enhanced compatibility and usability.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,6 @@
 
 ## 1.0.1
 - Readme update
+
+## 1.0.2
+- Improved package to address Pub Points issues for enhanced compatibility and usability.
--- lib/get_connect_logger.dart
+++ lib/get_connect_logger.dart
@@ -5,11 +5,13 @@ import 'package:get/get_connect.dart' as g;
 import 'package:get_connect_logger/models/get_connect_logger_settings.dart';
 import 'package:get_connect_logger/src/logger/logger.dart';
 
+/// A logger for GetConnect HTTP requests and responses.
 class GetConnectLogger {
   GetConnectLogger._();
 
   static GetConnectLogger? _instance;
 
+  /// Singleton instance of [GetConnectLogger].
   static GetConnectLogger get instance {
     _instance ??= GetConnectLogger._();
     return _instance!;
@@ -17,6 +19,21 @@ class GetConnectLogger {
 
   bool _enabled = false;
 
+  /// Enables logging for HTTP requests and responses.
+  ///
+  /// When called, this method activates logging for all HTTP requests and responses
+  /// made through the provided [httpClient]. If [settings] are provided, they can
+  /// be used to configure the logger behavior.
+  ///
+  /// Only activates logging in debug mode.
+  ///
+  /// Example:
+  /// ```dart
+  /// GetConnectLogger.instance.enableLogger(
+  ///   GetHttpClient(), // Provide your GetConnect HTTP client instance
+  ///   settings: GetConnectLoggerSettings(), // Optional settings
+  /// );
+  /// ```
   void enableLogger(
     g.GetHttpClient httpClient, {
     GetConnectLoggerSettings? settings,
